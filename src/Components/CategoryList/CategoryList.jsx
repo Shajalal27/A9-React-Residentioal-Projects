487ef7650daa5f4e70386d71e7585d8bcb327874
Shajalal27 <hpshajalal@gmail.com>
2024-04-10 08:34:52 +0600

    footer section added

--- src/Components/CategoryList/CategoryList.jsx
+++ src/Components/CategoryList/CategoryList.jsx
@@ -1,16 +1,28 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
+
 
 
 const CategoryList = ({cardIteam}) => {
-    const{image, title} = cardIteam
+    const{image, title, price, description, status, area} = cardIteam;
     return (
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
-                <figure><img className="h-40 w-full" src={image} alt="Shoes" /></figure>
+        <div className="card card-compact w-96 bg-base-100 shadow-xl border">
+                <figure><img className="h-[50vh] w-full rounded-lg" src={image} alt="Shoes" /></figure>
                 <div className="card-body">
+                    <h4 className="text-red-400 font-bold">{price}</h4>
                     <h2 className="card-title">{title}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    { 
+                        <p>{description.slice(0, 100)} </p> 
+                       
+                    }
+                    <div className="flex gap-28 justify-between">
+                        <p className="text-[#f59e0b] bg-yellow-100 font-bold p-1  rounded-lg
+                       
+                        ">{status}</p>
+                        <p className="text-[#329b3e] bg-[#e7e5b9] font-bold
+                        p-1  rounded-lg">{area}</p>
+                    </div>
+                    <div className="card-actions ">
+                    <button className="btn btn-success w-full font-bold mt-4">View Property</button>
                     </div>
                 </div>
         </div>
--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,15 +1,17 @@
 import {Outlet} from "react-router-dom";
 import Navbar from "../Components/Navbar/Navbar";
 import AuthProvider from "../AuthProvider/AuthProvider";
+import Footer from "../Components/Footer/Footer";
 
 const Root = () => {
     return (
-        <div className="max-w-6xl mx-auto">
+        <div className="max-w-7xl mx-auto">
             <Navbar></Navbar>
             <AuthProvider text={'hello'}>
 
             </AuthProvider>
             <Outlet></Outlet>
+            <Footer></Footer>
            
         </div>
     );
